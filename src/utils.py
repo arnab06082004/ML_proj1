2e720f8ac56188d29c47a8e4380edf31648392ec
Arnab <arnabmazumdar2020@gmail.com>
2025-11-11 14:38:12 +0530

    adding prdicted pipeline

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,9 @@
 import os
 import sys
-import numpy as np
-import pandas as pd
 import dill
 from src.exception import CustomException
+from sklearn.metrics import r2_score
+
 
 def save_object(file_path, obj):
     try:
@@ -15,3 +15,33 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+
+
+def evaluate_model(X_train, X_test, y_train, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)
+
+            # Predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Evaluation
+            train_score = r2_score(y_train, y_train_pred)
+            test_score = r2_score(y_test, y_test_pred)
+
+            report[model_name] = test_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file:
+            return dill.load(file)
+    except Exception as e:
+        raise CustomException(e,sys)
